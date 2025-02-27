4944b9774ecd92422061312e5c5bbc923978c082
hqahaad <grabq04@gmail.com>
2025-02-27 14:58:48 +0900

    [ Fix ] 산소, 엑사노바

--- Assets/Scripts/UI/FailPanelUI.cs
+++ Assets/Scripts/UI/FailPanelUI.cs
@@ -1,8 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FailPanelUI : MonoBehaviour
 {
+    [SerializeField] Button retryButton;
 
+    void Start()
+    {
+        retryButton.onClick.AddListener(() =>
+        {
+            SceneLoadManager.Instance.ReloadScene();
+        });
+    }
 }
